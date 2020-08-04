ad37f4a530c675fbbf66252430d69bd6d1227c39
Michael Hoy <mjh@mjhoy.com>
2020-08-03 20:24:48 -0400

    05: rainbow

--- src/05-led-roulette/src/main.rs
+++ src/05-led-roulette/src/main.rs
@@ -2,14 +2,28 @@
 #![no_main]
 #![no_std]
 
-use aux5::entry;
+use aux5::{entry, prelude::*, Delay, Leds};
 
 #[entry]
 fn main() -> ! {
-    let _y;
-    let x = 42;
-    _y = x;
+    let (mut delay, mut leds): (Delay, Leds) = aux5::init();
 
-    // infinite loop; just so we don't leave this stack frame
-    loop {}
+    let period = 50_u16;
+
+    let mut led_1: usize = 0;
+    let mut led_2: usize = 1;
+
+    loop {
+        leds[led_2].on();
+        delay.delay_ms(period);
+        leds[led_1].off();
+        delay.delay_ms(period);
+
+        led_1 = led_2;
+        if led_2 == 7 {
+            led_2 = 0
+        } else {
+            led_2 += 1;
+        }
+    }
 }
