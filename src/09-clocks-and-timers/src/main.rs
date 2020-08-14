acb1b88106b26eb2a08053afb5aae262d7881ec9
DESKTOP-QGPN99S\Mike <mikedeff@gmail.com>
2020-08-13 21:06:50 -0400

    chapter 9 completed

--- src/09-clocks-and-timers/src/main.rs
+++ src/09-clocks-and-timers/src/main.rs
@@ -5,14 +5,45 @@ use aux9::{entry, tim6};
 
 #[inline(never)]
 fn delay(tim6: &tim6::RegisterBlock, ms: u16) {
-    // TODO implement this
+    // Set the timer to go off in `ms` ticks
+    // 1 tick = 1 ms
+    tim6.arr.write(|w| w.arr().bits(ms));
+
+    // CEN: Enable the counter
+    tim6.cr1.modify(|_, w| w.cen().set_bit());
+
+    // Wait until the alarm goes off (until the update event occurs)
+    while !tim6.sr.read().uif().bit_is_set() {}
+
+    // Clear the update event flag
+    tim6.sr.modify(|_, w| w.uif().clear_bit())
 }
 
 #[entry]
 fn main() -> ! {
     let (mut leds, rcc, tim6) = aux9::init();
 
-    // TODO initialize TIM6
+    // Power on the TIM6 timer
+    rcc.apb1enr.modify(|_, w| w.tim6en().set_bit());
+
+    // SR, the status register.
+    // EGR, the event generation register.
+    // CNT, the counter register.
+    // PSC, the prescaler register.
+    // ARR, the autoreload register.
+
+    // CR1 Control Register 1
+    // OPM Select one pulse mode
+    // CEN Counter Enable - Keep the counter disabled for now
+    tim6.cr1.write(|w| w.opm().set_bit().cen().clear_bit());
+
+    // Configure the prescaler to have the counter operate at 1 KHz
+    // PSC Pre-scaler
+    // Remember that the frequency of the counter is apb1 / (psc + 1) and that apb1 is 8 MHz.
+    // APB1_CLOCK = 8 MHz
+    // 8 MHz / (7999 + 1) = 1 KHz
+    // The counter (CNT) will increase on every millisecond
+    tim6.psc.write(|w| w.psc().bits(7999));
 
     let ms = 50;
     loop {
