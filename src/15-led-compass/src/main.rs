8169c64d3f57519dc1fd8c228f698f7e0a82cfb3
Michael Hoy <mjh@mjhoy.com>
2020-09-04 07:28:58 -0400

    magnitude

--- src/15-led-compass/src/main.rs
+++ src/15-led-compass/src/main.rs
@@ -2,59 +2,28 @@
 #![no_main]
 #![no_std]
 
-// You'll find this useful ;-)
-use core::f32::consts::PI;
-
 #[allow(unused_imports)]
-use aux15::{entry, iprint, iprintln, prelude::*, Direction, I16x3};
-// this trait provides the `atan2` method
+use aux15::{entry, iprint, iprintln, prelude::*, I16x3};
 use m::Float;
 
 #[entry]
 fn main() -> ! {
-    let (mut leds, mut lsm303dlhc, mut delay, mut itm) = aux15::init();
+    const XY_GAIN: f32 = 1100.; // LSB / G
+    const Z_GAIN: f32 = 980.; // LSB / G
+
+    let (_leds, mut lsm303dlhc, mut delay, mut itm) = aux15::init();
 
     loop {
-        let I16x3 { x, y, .. } = lsm303dlhc.mag().unwrap();
+        let I16x3 { x, y, z } = lsm303dlhc.mag().unwrap();
 
-        let theta = (y as f32).atan2(x as f32); // in radians
+        let x = f32::from(x) / XY_GAIN;
+        let y = f32::from(y) / XY_GAIN;
+        let z = f32::from(z) / Z_GAIN;
 
-        iprintln!(&mut itm.stim[0], "theta: {}", theta);
+        let mag = (x * x + y * y + z * z).sqrt();
 
-        // FIXME pick a direction to point to based on `theta`
-        let led_angle = (2.0 * PI) / 8.0;
-        let offset = led_angle / 2.0;
-        let dir = match theta {
-            x if x < offset && x > -offset => Direction::South,
-            x if x < (led_angle + offset) && x > (led_angle - offset) => Direction::Southeast,
-            x if x < ((2.0 * led_angle) + offset) && x > ((2.0 * led_angle) - offset) => {
-                Direction::East
-            }
-            x if x < ((3.0 * led_angle) + offset) && x > ((3.0 * led_angle) - offset) => {
-                Direction::Northeast
-            }
-            x if x < ((4.0 * led_angle) + offset) && x > ((4.0 * led_angle) - offset) => {
-                Direction::North
-            }
-            // overflow
-            x if x < ((-4.0 * led_angle) + offset) && x > ((-4.0 * led_angle) - offset) => {
-                Direction::North
-            }
-            x if x < ((-3.0 * led_angle) + offset) && x > ((-3.0 * led_angle) - offset) => {
-                Direction::Northwest
-            }
-            x if x < ((-2.0 * led_angle) + offset) && x > ((-2.0 * led_angle) - offset) => {
-                Direction::West
-            }
-            x if x < ((-1.0 * led_angle) + offset) && x > ((-1.0 * led_angle) - offset) => {
-                Direction::Southwest
-            }
-            _ => Direction::North,
-        };
+        iprintln!(&mut itm.stim[0], "{} mG", mag * 1_000.);
 
-        leds.iter_mut().for_each(|led| led.off());
-        leds[dir].on();
-
-        delay.delay_ms(100_u8);
+        delay.delay_ms(500_u16);
     }
 }
