ce2073905ec7943f8cc47304500d9f85e25ea0b8
DESKTOP-QGPN99S\Mike <mikedeff@gmail.com>
2020-09-16 20:07:25 -0400

    chapter 14 completed

--- src/14-i2c/src/main.rs
+++ src/14-i2c/src/main.rs
@@ -19,23 +19,48 @@ fn main() -> ! {
     // Stage 1: Send the address of the register we want to read to the
     // magnetometer
     {
-        // TODO Broadcast START
+        // Broadcast START
+        // Broadcast the MAGNETOMETER address with the R/W bit set to Write
+        i2c1.cr2.write(|w| {
+            w.sadd1().bits(MAGNETOMETER); //secondary's address
+            w.rd_wrn().clear_bit(); //primary requests a write transfer
+            w.nbytes().bits(1); //primary requests 1 byte
+            w.start().set_bit(); //primary sets START to start generating message
+            w.autoend().clear_bit() //primary sets AUTOEND off = SW end mode, to require a STOP condition from SW
+        });
 
-        // TODO Broadcast the MAGNETOMETER address with the R/W bit set to Write
+        // Wait until we can send more data
+        while i2c1.isr.read().txis().bit_is_clear() {}
+        //ISR = Interrupt and Status Register
+        //TXIS = Transmit Interrupt Status, cleared by HW
 
-        // TODO Send the address of the register that we want to read: IRA_REG_M
+        // Send the address of the register that we want to read: IRA_REG_M
+        i2c1.txdr.write(|w| w.txdata().bits(IRA_REG_M));
+        //TXDR = Transmit Data Register
+
+        // Wait until the previous byte has been transmitted
+        while i2c1.isr.read().tc().bit_is_clear() {}
+        //TC = Transfer Complete
     }
 
     // Stage 2: Receive the contents of the register we asked for
     let byte = {
-        // TODO Broadcast RESTART
+        // Broadcast RESTART
+        // Broadcast the MAGNETOMETER address with the R/W bit set to Read
+        i2c1.cr2.modify(|_, w| {
+            w.nbytes().bits(1); //primary requests a write transfer
+            w.rd_wrn().set_bit(); //primary requests 1 byte
+            w.start().set_bit(); //primary sets START to start generating message
+            w.autoend().set_bit() //primary sets AUTOEND on = automatic HW end mode
+        });
 
-        // TODO Broadcast the MAGNETOMETER address with the R/W bit set to Read
+        // Wait until we have received the contents of the register
+        while i2c1.isr.read().rxne().bit_is_clear() {}
+        //RXNE = Receive Data Register Not Empty, cleared by HW
 
-        // TODO Receive the contents of the register
-
-        // TODO Broadcast STOP
-        0
+        // Receive the contents of the register
+        i2c1.rxdr.read().rxdata().bits()
+        //RXDR = Receive Data Register
     };
 
     // Expected output: 0x0A - 0b01001000
